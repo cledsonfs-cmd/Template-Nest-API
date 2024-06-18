2d2f05ddf0b614fa61803aa12adb3788d0900788
Cledson Francisco Silva <70558441+cledsonfs-cmd@users.noreply.github.com>
2024-06-17 21:51:41 -0300

    fix: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3000);
+// }
+// bootstrap();
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+ const app = await NestFactory.create(AppModule);
+ app.useGlobalPipes(new ValidationPipe());
+
+ const config = new DocumentBuilder()
+   .setTitle('Api Nest - Template Base')
+   .setDescription('Templeta basico para desenvolvimento de APIs em Java.')
+   .setVersion('1.0')
+   .setContact('Cledson Francisco Silva', 'www.cledsonfs.com.br', 'cledsonfs@gmail.com')
+   .build();
+ const document = SwaggerModule.createDocument(app, config);
+ SwaggerModule.setup('api', app, document);
+
+ await app.listen(3000);
 }
 bootstrap();
